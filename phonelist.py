155b43f256b2724e3a430e81a354bf80e8652286
Melody Kramer <melodykramer@gmail.com>
2014-02-28 18:23:33 -0500

    Update phonelist.py

--- phonelist.py
+++ phonelist.py
@@ -17,4 +17,25 @@ with open('social22814.csv', "rU") as employee_social_survey:
 
  	for employee in survey:
 		values = employee.values()
-		print values
+		#print values
+
+	#for employee in survey:
+		text = values[4]
+		print text
+
+	#if employee.values() == individual.values():
+	#	print "Hooray!"
+	#else:
+	#	print "Back to the drawing board Kramer!"
+
+
+
+
+
+
+
+#employees_plus_survey = file('results.csv', 'w')
+
+#master_document = csv.writer(employees_plus_survey)
+
+
