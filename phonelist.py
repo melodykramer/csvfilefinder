f8a7169be793081de8036c0da0b46f23371acb5c
Melody Kramer <melodykramer@gmail.com>
2014-03-03 12:55:56 -0500

    Update phonelist.py

--- phonelist.py
+++ phonelist.py
@@ -11,23 +11,35 @@ with open('npremployees.csv', "rU") as npr_employees:
 
        all_employees = {}
 
+       total_employees = {}
 
-#loops through all of the employees and assigns them the value of their extension
+
+#loops through all of the employees and assigns them a unique value of their extension
 
        for employee in employees:
 
                all_employees[employee['Extension']] = employee
 
 
+#opens up spreadsheet of NPR employees who filled out a survey
 
 with open('social22814.csv', "rU") as social_employees:
 
+#puts all of their information into a dictionary
+
        social_employee = csv.DictReader(social_employees) 
+
+
+#if the employee appears in the master list, print out their information. 
+#This lets us know which employees should have information added to the master list entry.
 
        for row in social_employee:
 
+          # total_employees[row['Extension']]['Instagram'] = row
+
+             #  print total_employees.get(row['Extension'], None)
+            	
+
                print all_employees.get(row['Extension'], None)
 
-       for employee_new in employees:
 
-   	 	all_employees[employee_new['Extension']]['Instagram'] = employee_new
